53eae826b8f6070d64134de04eaab1b72b290ab8
Jouni Kaitasalo <jouni.kaitasalo@hotmail.com>
2023-09-26 17:41:17 +0300

    ViewShoppinglists.jsx first draft

---
+++ src/components/ViewShoppinglists.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { API_URL } from "../constants";
+
+function ViewShoppinglists() {
+  const [shoppinglists, setShoppinglists] = useState([]);
+
+  useEffect(() => {
+    loadShoppinglists();
+  }, []);
+
+  const loadShoppinglists = async () => {
+    try {
+      const result = await axios.get(`${API_URL}/shoppinglists`);
+      setShoppinglists(result.data);
+    } catch (error) {
+      console.error("Error loading shoppinglists:", error);
+    }
+  };
+
+  const shoppinglistItems = shoppinglists.map((shoppinglist) => (
+    <div key={shoppinglist.id} className="shoppinglist-item">
+      <h1>{shoppinglist.name}</h1>
+      <p>{shoppinglist.details}</p>
+      <Link to={`/shoppinglist/${shoppinglist.id}`}>View Shoppinglist</Link>
+    </div>
+  ));
+
+  return (
+    <div className="view-shoppinglists">
+      <h1>All Shoppinglists</h1>
+      <div className="shoppinglist-container">{shoppinglistItems}</div>
+    </div>
+  );
+}
+
+export default ViewShoppinglists;
