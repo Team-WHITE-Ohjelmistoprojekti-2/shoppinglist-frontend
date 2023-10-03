2e4e934e19fc0f31ada129b5ce0842272ee8cd3a
Juuso Hakala <juuso.hakala@myy.haaga-helia.fi>
2023-10-03 16:41:58 +0300

    improvements to docker stuff

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -33,55 +33,57 @@ function AddProduct() {
 
   return (
     <div className="add-form">
-    <h2>Add Product</h2>
-    <form onSubmit={(e) => onSubmit(e)}>
-      <div className="form-group">
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          placeholder="Product name"
-          name="name"
-          value={name}
-          onChange={(e) => onInputChange(e)}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="quantity">Quantity</label>
-        <input
-          type="text"
-          id="quantity"
-          placeholder="Quantity"
-          name="quantity"
-          value={quantity}
-          onChange={(e) => onInputChange(e)}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="price">Price</label>
-        <input
-          type="text"
-          id="price"
-          placeholder="Price"
-          name="price"
-          value={price}
-          onChange={(e) => onInputChange(e)}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="details">Details</label>
-        <input
-          type="text"
-          id="details"
-          placeholder="Details"
-          name="details"
-          value={details}
-          onChange={(e) => onInputChange(e)}
-        />
-      </div>
-      <button type="submit">Add Product</button>
-      <button type="button" onClick={handleCancel}>Cancel</button>
-    </form>
+      <h2>Add Product</h2>
+      <form onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            placeholder="Product name"
+            name="name"
+            value={name}
+            onChange={(e) => onInputChange(e)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="quantity">Quantity</label>
+          <input
+            type="text"
+            id="quantity"
+            placeholder="Quantity"
+            name="quantity"
+            value={quantity}
+            onChange={(e) => onInputChange(e)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="price">Price</label>
+          <input
+            type="text"
+            id="price"
+            placeholder="Price"
+            name="price"
+            value={price}
+            onChange={(e) => onInputChange(e)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="details">Details</label>
+          <input
+            type="text"
+            id="details"
+            placeholder="Details"
+            name="details"
+            value={details}
+            onChange={(e) => onInputChange(e)}
+          />
+        </div>
+        <button type="submit">Add Product</button>
+        <button type="button" onClick={handleCancel}>
+          Cancel
+        </button>
+      </form>
     </div>
   );
 }
