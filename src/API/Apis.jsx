f6729810b71600619ee13a9d4c0ad1b871e73683
Juuso Hakala <juuso.hakala@myy.haaga-helia.fi>
2023-11-09 16:24:31 +0200

    user registration functionality

--- src/API/Apis.jsx
+++ src/API/Apis.jsx
@@ -11,50 +11,64 @@ export const getShoppinglistById = async (id) => {
     console.error(`Error fetching shoppinglist with ID ${id}:`, error);
     throw error; // Re-throw the error to handle it elsewhere, if necessary
   }
-}
+};
+
 export const getShoppinglists = async () => {
-    try{
-        const result = await axios.get(`${API_URL}/shoppinglists`);
+  try {
+    const result = await axios.get(`${API_URL}/shoppinglists`);
     return result.data;
-    } catch (error) {
-        // Handle error if needed
-        console.error('Error fetching shoppinglists:', error);
-        throw error; // Re-throw the error to handle it elsewhere, if necessary
-      }
-}
+  } catch (error) {
+    // Handle error if needed
+    console.error("Error fetching shoppinglists:", error);
+    throw error; // Re-throw the error to handle it elsewhere, if necessary
+  }
+};
 
 export async function deleteShoppinglist(id) {
-    try {
-      const result =  await axios.delete(`${API_URL}/shoppinglists/${id}`);
-      return result.data;
-    } catch (error) {
-      // Handle error if needed
-      console.error('Error deleting post:', error);
-      throw error; // Re-throw the error to handle it elsewhere, if necessary
-    }
+  try {
+    const result = await axios.delete(`${API_URL}/shoppinglists/${id}`);
+    return result.data;
+  } catch (error) {
+    // Handle error if needed
+    console.error("Error deleting post:", error);
+    throw error; // Re-throw the error to handle it elsewhere, if necessary
   }
-  export async function addShoppinglist(list) {
-    try {
-      const result = await axios.post(`${API_URL}/shoppinglists`, list, {
-      });
-      return result.data;
-    } catch (error) {
-      // Handle error if needed
-      console.error("Error creating list:", error);
-      throw error; // Re-throw the error to handle it elsewhere, if necessary
-    }
-  }
+}
 
-  export async function editShoppinglist(updatedList) {
-    try {
-      const result = await axios.put(
-        `${API_URL}/shoppinglists/${updatedList.id}`,
-        { name: updatedList.updatedList.name }
-      );
-      return result.data;
-    } catch (error) {
-      // Handle error if needed
-      console.error("Error updating list:", error);
-      throw error; // Re-throw the error to handle it elsewhere, if necessary
-    }
+export async function addShoppinglist(list) {
+  try {
+    const result = await axios.post(`${API_URL}/shoppinglists`, list, {});
+    return result.data;
+  } catch (error) {
+    // Handle error if needed
+    console.error("Error creating list:", error);
+    throw error; // Re-throw the error to handle it elsewhere, if necessary
   }
+}
+
+export async function editShoppinglist(updatedList) {
+  try {
+    const result = await axios.put(
+      `${API_URL}/shoppinglists/${updatedList.id}`,
+      { name: updatedList.updatedList.name }
+    );
+    return result.data;
+  } catch (error) {
+    // Handle error if needed
+    console.error("Error updating list:", error);
+    throw error; // Re-throw the error to handle it elsewhere, if necessary
+  }
+}
+
+export async function signupUser(signupData) {
+  try {
+    const result = await axios.post(
+      `${API_URL}/registration/register`,
+      signupData
+    );
+    return result;
+  } catch (error) {
+    console.error("Failed to signup user:", error);
+    throw error;
+  }
+}
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,24 +1,75 @@
 import { TextField, Flex, Text, Button, Theme, Card } from "@radix-ui/themes";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useNavigate } from "react-router-dom";
+import { signupUser } from "../API/Apis";
 
 function SignUp() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const [errorMsg, setErrorMsg] = useState('');
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: signupUser,
+    onSuccess: (data) => {
+      if (data.status === 200) {
+        console.log('success bro!')
+        queryClient.invalidateQueries(["registration"]);
+        navigate(`/`);
+      }
+    },
+    onError: (err) => {
+      console.log(err.response.data);
+      setErrorMsg(err.response.data.map(i => i + '. '));
+    },
+  });
+
+  const onSubmit = () => {
+    console.log(formData);
+    mutate(formData);
+  };
+
   return (
     <div>
       <Theme>
         <Card size="4" style={{ maxWidth: 275 }}>
           <Flex direction="column" gap="1" style={{ maxWidth: 275 }}>
-            <Text align="center" mb="4" size="5" weight="bold">Create a new account</Text>
+            <Text align="center" mb="4" size="5" weight="bold">
+              Create a new account
+            </Text>
             <Text align="left" size="2" weight="bold">
               Username
             </Text>
-            <TextField.Input size="2" placeholder="" />
+            <TextField.Input
+              size="2"
+              placeholder=""
+              onChange={(e) =>
+                setFormData({ ...formData, username: e.target.value })
+              }
+            />
             <Text align="left" size="2" weight="bold">
               Password
             </Text>
-            <TextField.Input size="2" placeholder="" type="password" />
-            <Text>Maybe validation error here. For example: Username already taken</Text>
-            <Button color="crimson" mt="4">Create Account</Button>
-            <Text align="center" mt="2" as="span">Already have an account? <Link to="/login">Sign In</Link></Text>
+            <TextField.Input
+              size="2"
+              placeholder=""
+              type="password"
+              onChange={(e) =>
+                setFormData({ ...formData, password: e.target.value })
+              }
+            />
+            <Text>{ errorMsg }</Text>
+            <Button color="crimson" mt="4" onClick={() => onSubmit()}>
+              Create Account
+            </Button>
+            <Text align="center" mt="2" as="span">
+              Already have an account? <Link to="/login">Sign In</Link>
+            </Text>
           </Flex>
         </Card>
       </Theme>
