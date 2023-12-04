1a18d2b8698505e5162c0937924adb03b68c3a78
Juuso Hakala <juuso.hakala@myy.haaga-helia.fi>
2023-12-04 12:01:16 +0200

    API_URL can be loaded from environment variable

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+
+//export const API_URL = "https://haagaheliashoppinglist-c2c43878d673.herokuapp.com/api"
+
+export const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:8080/api";
--- src/constants.jsx
+++
@@ -1,6 +0,0 @@
-export const PRODUCTS_URL = "http://localhost:8080/api/products"
-export const PRODUCT_URL = "http://localhost:8080/api/product"
-//export const API_URL = "https://haagaheliashoppinglist-c2c43878d673.herokuapp.com/api"
-
-// Local backend for development
-export const API_URL = "http://localhost:8080/api"
